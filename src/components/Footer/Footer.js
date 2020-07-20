c27a727685bdf51710da2204cc3cb5febec14ae4
maneesh_p <hellomaneeshp@gmail.com>
2020-07-20 17:10:03 +0530

    fixed footer issue

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,13 +4,15 @@ import "./footer.css";
 function Footer() {
   return (
     <nav class="nav sticky footer">
-      <ul class="nav mt-3 mb-3">
-        <li class="nav-item">
-          <a class="nav-link" id="footer-link">
-            Copyright &copy; 2010-2017 Tripwiz
-          </a>
-        </li>
-      </ul>
+      <div className="container">
+        <ul class="nav mt-3 mb-3">
+          <li class="nav-item">
+            <a class="nav-link" id="footer-link">
+              Copyright &copy; 2010-2017 Tripwiz
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
